cbf311e44b736a58aca1e826a3f67432a988b6bd
carambole25 <carambole258@gmail.com>
2025-01-23 14:48:47 +0100

    gestion de la generation des clé depuis l'ui

---
+++ infra/ui/generate_api_key.php
@@ -0,0 +1,43 @@
+<?php
+$host = 'db';
+$dbname = 'eagle_db';
+$username = 'changeme';
+$password = 'changeme';
+
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+if (isset($_GET['key'])) {
+    $generated_key = htmlspecialchars($_GET['key']);
+    $sql = "INSERT INTO api_keys (api_key) VALUES (?)";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute([$generated_key]);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Titre de la page</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <a id="api_key" href="">Run PHP Function</a>
+    </body>
+</html>
+
+<script type="text/javascript">
+    let api_key = Math.random().toString(36).substr(2, 10)+Math.random().toString(36).substr(2, 10);
+    document.getElementById("api_key").href="generate_api_key.php?key="+api_key; 
+</script>
--- infra/ui/index.php
+++ infra/ui/index.php
@@ -1,5 +1,12 @@
-<?php
-
-echo "yolo !";
-
-?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Titre de la page</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <a href="generate_api_key.php">Generer des clés API</a>
+    </body>
+</html>
